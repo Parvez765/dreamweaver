7d05aa6e9bc65ed647d17baceaba7cf619b4648e
parvez <pervezhossain1998@gmail.com>
2023-09-06 10:22:33 +0600

    Latest Love Added

--- src/Pages/CouplePage/CouplePage.js
+++ src/Pages/CouplePage/CouplePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CouplesComponent from '../../Components/CouplesComponent/CouplesComponent';
+import LatestLove from '../../Components/HomeComponent/LatestLove/LatestLove';
 
 const CouplePage = () => {
     return (
         <div>
             <CouplesComponent/>
+            <LatestLove/>
         </div>
     );
 };
--- src/Pages/MomentPage/MomentPage.js
+++ src/Pages/MomentPage/MomentPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MomentsComponent from '../../Components/MomentsComponent/MomentsComponent';
+import LatestLove from '../../Components/HomeComponent/LatestLove/LatestLove';
 
 const MomentPage = () => {
     return (
         <div>
             <MomentsComponent/>
+            <LatestLove/>
         </div>
     );
 };
