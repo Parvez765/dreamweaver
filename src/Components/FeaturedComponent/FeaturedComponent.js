cd771490528e455852353a6f3bd445607c966b76
parvez <pervezhossain1998@gmail.com>
2023-09-06 07:27:42 +0600

    Featured Project Added

--- src/Components/FeaturedComponent/FeaturedComponent.js
+++ src/Components/FeaturedComponent/FeaturedComponent.js
@@ -11,12 +11,11 @@ const FeaturedComponent = () => {
             <h1 className='mt-[80px] text-[30px] lg:text-[40px] text-white'>Featured Projects</h1>
             <div className="grid lg:grid-cols-2 gap-[70px] mt-[80px]">
                 {
-                    projects?.map(project =>
+                   projects?.map((project) =>
                     <div>
                         <Link to={`/featured/${project.id}`}><img src={project.img} alt="Latest Projects" className='lg:h-[1000px] w-full object-cover projectImage rounded-lg'/></Link>
                     </div>)
                 }
-               
             </div>
         </div>
     );
--- src/Components/HomeComponent/LatestLove/LatestLove.js
+++ src/Components/HomeComponent/LatestLove/LatestLove.js
@@ -8,7 +8,7 @@ const LatestLove = () => {
                 <h1 className='text-white mt-[100px] text-[30px] lg:text-[40px] mb-[30px]'>Latest Love On Facebook And Instagram</h1>
                 <div className='grid lg:grid-cols-6 gap-[15px]'>
                   {
-                    latestdata?.map(lData => <div>
+                    latestdata?.slice(0, 6).map(lData => <div>
                         <img src={lData.img} alt="DreamWeaver Latest Projects" className='lg:w-[300px] lg:h-[300px] rounded-lg hover:border border-xl cursor-pointer'/>
                     </div>)
                   }
